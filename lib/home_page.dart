6a022a7b1599b8f26c83b3b715132644438a53dd
TFDNINE9 <ekthaviphonhthongphet@gmail.com>
2024-06-14 11:17:12 +0700

    edit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -140,7 +140,7 @@ class _HomePageState extends State<HomePage> {
                       foregroundColor: Colors.black,
                       shape: const StadiumBorder(),
                     ),
-                    child: const Text('Stop being gay'),
+                    child: const Text('food'),
                   ),
                 ],
               ),
